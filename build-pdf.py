48e80cfa2826fd9b7a103b043b91aaf77810dfe4
Lars Kotthoff <larsko@cs.ubc.ca>
2016-08-29 13:45:18 -0700

    headers

--- build-pdf.py
+++ build-pdf.py
@@ -4,7 +4,12 @@ import re
 import sys
 
 os.chdir("docs")
-os.system("echo -e '---\ntitle: mlr Tutorial\n---' > ../mlr-tutorial.md")
+with open(os.path.join("..", "mlr-tutorial.md"), "w") as fd:
+  fd.write("""
+---
+title: mlr Tutorial
+---
+""")
 os.system("for f in `grep -o \"[^']\+\.md\" ../mkdocs.yml`; do (cat \"${f}\"; echo) >> ../mlr-tutorial.md; done")
 os.chdir("..")
 
@@ -13,12 +18,12 @@ def link_fixer(match):
   with open(os.path.join("docs", file), 'r') as fd:
     return "(" + re.sub(' ', '-', re.sub(' ', '', fd.readline().rstrip(), 1)).lower() + ")"
 
-with open('mlr-tutorial.md', 'r+') as file:
-  data = file.read()
+with open('mlr-tutorial.md', 'r+') as fd:
+  data = fd.read()
   out = re.sub(r'\(([a-zA-Z0-9_]+\.md)\)', link_fixer, data)
-  file.seek(0)
-  file.write(out)
-  file.truncate()
+  fd.seek(0)
+  fd.write(out)
+  fd.truncate()
 
 retval = os.system("pandoc --latex-engine=xelatex --variable colorlinks=\"true\" --listings -H latex-setup.tex --toc -f markdown+grid_tables+table_captions-implicit_figures -o mlr-tutorial.pdf mlr-tutorial.md")
 if retval != 0:
