28f637baf296052facb5c47e4ec491d1f248e876
schiffner <schiffner@math.uni-duesseldorf.de>
2017-04-07 21:27:24 +0200

    try to fix Travis (fill package cache)

--- r_packs_install.R
+++ r_packs_install.R
@@ -19,6 +19,6 @@ pmiss = setdiff(p, ip2)
 print("MISSING PACKAGES:")
 print(pmiss)
 if (length(pmiss) > 0)
-  install.packages(pmiss)
+  install.packages(pmiss[1:20])
 
 update.packages(ask = FALSE)
