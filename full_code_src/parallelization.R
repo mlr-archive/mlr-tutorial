54dfd1414fb55cd044d5ca2de707f5e41b3e9ff7
Steve Bronder <stevo15025@gmail.com>
2017-11-10 16:47:48 -0500

    delete full_code_src folder

--- full_code_src/parallelization.R
+++
@@ -1,8 +0,0 @@
-library("parallelMap")
-parallelStartSocket(2)
-
-rdesc = makeResampleDesc("CV", iters = 3)
-r = resample("classif.lda", iris.task, rdesc)
-
-parallelStop()
-parallelGetRegisteredLevels()
